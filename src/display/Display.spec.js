18cc5b58bfb706e3b14599f507afe9be8800907a
Jess <jessdev83@gmail.com>
2019-02-05 22:18:12 -0500

    Display tests
    Tests written for display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,4 +1,28 @@
 import React from "react";
-import { render } from "react-testing-library";
+import { render, cleanup } from "react-testing-library";
 import "jest-dom/extend-expect";
 import Display from "./Display";
+
+afterEach(cleanup);
+
+describe("the display component", () => {
+  const disp = render(<Display />);
+  const lockButton = disp.getByTestId("lock");
+  const openButton = disp.getByTestId("open");
+  describe("the lock button", () => {
+    it("is green at pageload", () => {
+      expect(lockButton).toHaveClass("green-led");
+    });
+    it("says unlocked at page load", () => {
+      expect(lockButton).toHaveTextContent(/unlocked/i);
+    });
+  });
+  describe("the open button", () => {
+    it("is green at pageload", () => {
+      expect(openButton).toHaveClass("green-led");
+    });
+    it("says open at page load", () => {
+      expect(openButton).toHaveTextContent(/open/i);
+    });
+  });
+});
