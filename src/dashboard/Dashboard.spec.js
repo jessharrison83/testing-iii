3fb469337dd9a9325baa7044f6184aca98b278b2
Jess <jessdev83@gmail.com>
2019-02-05 22:24:17 -0500

    Dashboard Display tests
    added click functionality tests for displays to dashboard tests. MVP complete.

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -9,6 +9,8 @@ describe("control button click functions", () => {
   const dash = render(<Dashboard />);
   const lockButton = dash.getByTestId("lockButton");
   const openButton = dash.getByTestId("openButton");
+  const lockDisplay = dash.getByTestId("lock");
+  const openDisplay = dash.getByTestId("open");
   describe("the lock/unlock button", () => {
     it("toggles between locked and unlocked when button is clicked", () => {
       fireEvent.click(openButton);
@@ -20,6 +22,28 @@ describe("control button click functions", () => {
     it("toggles between open and close gate when button is clicked", () => {
       fireEvent.click(openButton);
       expect(openButton).toHaveTextContent(/open gate/i);
+    });
+  });
+  describe("the lock/unlock display", () => {
+    it("changes to red when gate is locked", () => {
+      fireEvent.click(openButton);
+      fireEvent.click(lockButton);
+      expect(lockDisplay).toHaveClass("red-led");
+    });
+    it("says locked when gate is locked", () => {
+      fireEvent.click(openButton);
+      fireEvent.click(lockButton);
+      expect(lockDisplay).toHaveTextContent(/locked/i);
+    });
+  });
+  describe("the open/closed display", () => {
+    it("changes to red when closed", () => {
+      fireEvent.click(openButton);
+      expect(openDisplay).toHaveClass("red-led");
+    });
+    it("says closed when gate is closed", () => {
+      fireEvent.click(openButton);
+      expect(openDisplay).toHaveTextContent(/closed/i);
     });
   });
 });
